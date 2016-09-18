f8f2d8f42b5af1ed591b46752f8c17c361fe64e9
Henrique Shodi Maeta <japa1996@hotmail.com>
2016-09-18 17:59:25 -0300

    update

--- index.php
+++ index.php
@@ -127,8 +127,3 @@
             mysql_close($con);
         }
 ?>   
-<!-- Teremos uma plataforma de negociação de mercadorias. Que obrigatoriamente deverá ter os seguintes
-
-campos: Código da Mercadoria, Tipo da Mercadoria, Nome da Mercadoria, Quantidade, Preço, Tipo do Negócio
-
-(Compra ou Venda). -->
